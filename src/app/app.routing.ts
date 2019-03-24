b43e9edda1e16175c76763dbe4aff222308d5734
hugo <hugofsp93@hotmail.com>
2019-03-24 18:53:22 -0300

    header, sidebar, details ajusts + routing add

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,15 +2,16 @@ import { DetailComponent } from './detail/detail.component';
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
-import { FavoriteComponent } from './favorite/favorite.component';
-
 
 
 const APP_ROUTES: Routes = [
-      {path: '', component: HomeComponent},
-      {path: 'home', component: HomeComponent},
-      {path: 'details', component: DetailComponent},
-      {path: 'favorite', component: FavoriteComponent},
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'details', component: DetailComponent },
+  { path: 'services', component: DetailComponent },
+  { path: 'clients', component: DetailComponent },
+  { path: 'contact', component: DetailComponent },
+
 ];
 
 
